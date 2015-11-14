9c2bb15b7a3be0607d5e8e4e1b9d8f4544527cb0
Wisarn Patchoo <wisarn.p@bu.ac.th>
2015-11-14 15:00:40 +0700

    Sound Effect Successful ...

--- app/src/main/java/th/ac/bu/p/wisarn/butraffic/MainActivity.java
+++ app/src/main/java/th/ac/bu/p/wisarn/butraffic/MainActivity.java
@@ -1,13 +1,48 @@
 package th.ac.bu.p.wisarn.butraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind Widget
+        bindWidget();
+        buttonController();
+
+
+    }   // Main method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // sound effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(),R.raw.end_game);
+                buttonPlayer.start();
+
+                // Intent to WebView
+
+            }
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+    }
+
+}   // Main class นี่คือคลาสหลัก
